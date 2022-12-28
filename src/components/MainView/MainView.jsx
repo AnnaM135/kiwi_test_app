99c1039261b4f94ddf3bb57685b40fdf06bd17cd
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2022-12-28 14:11:14 +0400

    styles: some changeing in main view layout

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -4,13 +4,15 @@ export function MainView() {
     <div className={`${_._} container`}>
       <div className={_.__box}>component 1</div>
       <div className={_.__box}>component 2</div>
-      <div className={_.__box}>component 3</div>
-      <div className={_.__box}>component 4</div>
-      <div className={_.__box}>component 5</div>
-      <div className={_.__box}>component 6</div>
-      <div className={_.__box}>component 7</div>
-      <div className={_.__box}>component 8</div>
-      <div className={_.__box}>component 9</div>
+      <div className={_.__box}>
+        <div className={_.__box_area}>component 3</div>
+        <div className={_.__box_area}>component 4</div>
+        <div className={_.__box_area}>component 5</div>
+        <div className={_.__box_area}>component 6</div>
+        <div className={_.__box_area}>component 7</div>
+        <div className={_.__box_area}>component 8</div>
+        <div className={_.__box_area}>component 9</div>
+      </div>
     </div>
   );
 }
