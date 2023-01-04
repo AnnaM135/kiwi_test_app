78b5fbec0543797fa3737f210c17d8be74814cde
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2023-01-04 17:19:22 +0400

    change: changeing main layout styles

--- src/components/Coins/Coins.jsx
+++ src/components/Coins/Coins.jsx
@@ -6,21 +6,23 @@ export function Coins() {
   return (
     <div className={_._}>
       <TopTitle title="Coins" />
-      <Days>
-        <div className={_.__circle}>
-          <div className={`${_.__circle_top} ${_.__center}`}>
-            <span>TOP <span style={{color: '#61E224'}}>Gainer</span></span>
-            <span>Apecoin</span>
-            <span className={_.__circle_icon_gainer}>0.74%</span>
+      <div className={_.__center}>
+        <Days>
+          <div className={_.__circle}>
+            <div className={`${_.__circle_top} ${_.__center}`}>
+              <span>TOP <span style={{color: '#61E224'}}>Gainer</span></span>
+              <span>Apecoin</span>
+              <span className={_.__circle_icon_gainer}>0.74%</span>
+            </div>
+            <div className={_.__circle_wave}></div>
+            <div className={`${_.__circle_bottom} ${_.__center}`}>
+              <span>TOP <span style={{color: '#D23737'}}>Looser</span> </span>
+              <span>Apecoin</span>
+              <span className={_.__circle_icon_looser}>0.74%</span>
+            </div>
           </div>
-          <div className={_.__circle_wave}></div>
-          <div className={`${_.__circle_bottom} ${_.__center}`}>
-            <span>TOP <span style={{color: '#D23737'}}>Looser</span> </span>
-            <span>Apecoin</span>
-            <span className={_.__circle_icon_looser}>0.74%</span>
-          </div>
-        </div>
-      </Days>
+        </Days>
+      </div>
     </div>
   );
 }
